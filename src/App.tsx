6863ca263a8b43f0902bc7fb87c840255f40175a
Sayeed khan <80925930+sayeed1143@users.noreply.github.com>
2025-10-08 01:37:28 +0530

    Commit from Dualite Alpha: 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "next-themes";
 import Index from "./pages/Index";
 import Workspace from "./pages/Workspace";
 import CanvasMode from "./pages/CanvasMode";
@@ -15,25 +16,27 @@ import ChatPage from "./pages/ChatPage";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/workspace" element={<Workspace />} />
-          <Route path="/canvas" element={<CanvasMode />} />
-          <Route path="/practice" element={<PracticeMode />} />
-          <Route path="/doubt" element={<DoubtMode />} />
-          <Route path="/teacher" element={<TeacherDashboard />} />
-          <Route path="/chat" element={<ChatPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/workspace" element={<Workspace />} />
+            <Route path="/canvas" element={<CanvasMode />} />
+            <Route path="/practice" element={<PracticeMode />} />
+            <Route path="/doubt" element={<DoubtMode />} />
+            <Route path="/teacher" element={<TeacherDashboard />} />
+            <Route path="/chat" element={<ChatPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </ThemeProvider>
 );
 
 export default App;
